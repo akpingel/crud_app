b017ea32fdba44484be8dc92fb16db1a6bf69f16
Alyssa Pingel <alyssa.pingel@my.simpson.edu>
2021-02-25 09:34:09 -0600

    Assignment 5 changes

--- src/main/webapp/js/name_list.js
+++ src/main/webapp/js/name_list.js
@@ -46,9 +46,109 @@ function updateTable() {
                 htmlSafe(bdayString)+'</td></td></td></td></td></tr>');
         }
         $('#datatable tbody:first').remove();
+
+
         console.log("Done");
     });
 }
 
 // Call your code.
 updateTable();
+
+// Called when "Add Item" button is clicked
+function showDialogAdd() {
+
+    // Print that we got here
+    console.log("Opening add item dialog");
+
+    // Clear out the values in the form.
+    // Otherwise we'll keep values from when we last
+    // opened or hit edit.
+    // I'm getting it started, you can finish.
+    $('#id').val("");
+    $('#firstName').val("");
+    $('#lastName').val("");
+    $('#email').val("");
+    $('#phone').val("");
+    $('#birthday').val("");
+
+    // Show the hidden dialog
+    $('#myModal').modal('show');
+}
+
+// There's a button in the form with the ID "addItem"
+// Associate the function showDialogAdd with it.
+let addItemButton = $('#addItem');
+addItemButton.on("click", showDialogAdd);
+
+function saveChanges(){
+
+    console.log("Save changes test!!");
+    let firstName = $('#firstName').val();
+    console.log("First name: " + firstName);
+
+    let lastName = $('#lastName').val();
+    console.log("Last name: " + lastName);
+
+    let email = $('#email').val();
+    console.log("Email: " + email);
+
+    let phone = $('#phone').val();
+    console.log("Phone Number: " + phone);
+
+    let birthday = $('#birthday').val();
+    console.log("Birthday: " + birthday);
+
+    // Create the regular expression
+    let reg = /^[A-Za-z\u00C0-\u00FF'-(\s)]{1,10}$/;
+    //let reg = /^(\s)*[A-Za-z]+((\s)?((\'|\-|\.)?([A-Za-z])+))*(\s)*$/;
+    //let reg = /^[a-zA-Z\u00C0-\u00FF]*$/;
+
+    if (reg.test(firstName)) {
+        $('#firstName').removeClass("is-invalid");
+        $('#firstName').addClass("is-valid");
+    } else {
+        $('#firstName').removeClass("is-valid");
+        $('#firstName').addClass("is-invalid");
+    }
+
+    if (reg.test(lastName)) {
+        $('#lastName').removeClass("is-invalid");
+        $('#lastName').addClass("is-valid");
+    } else {
+        $('#lastName').removeClass("is-valid");
+        $('#lastName').addClass("is-invalid");
+    }
+
+    let regEmail = /^[a-zA-Z0-9-.]{1,15}@[A-Za-z-.]{1,15}.[A-Za-z]{3}$/;
+    if (regEmail.test(email)) {
+        $('#email').removeClass("is-invalid");
+        $('#email').addClass("is-valid");
+    } else {
+        $('#email').removeClass("is-valid");
+        $('#email').addClass("is-invalid");
+    }
+
+    let regPhone = /^\(?[1-9][0-9]{2}\)?-?[0-9]{3}-?[0-9]{4}$/;
+    if (regPhone.test(phone)) {
+        $('#phone').removeClass("is-invalid");
+        $('#phone').addClass("is-valid");
+    } else {
+        $('#phone').removeClass("is-valid");
+        $('#phone').addClass("is-invalid");
+    }
+
+    let regBirthday = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
+    if (regBirthday.test(birthday)) {
+        $('#birthday').removeClass("is-invalid");
+        $('#birthday').addClass("is-valid");
+    } else {
+        $('#birthday').removeClass("is-valid");
+        $('#birthday').addClass("is-invalid");
+    }
+
+
+}
+
+let saveChangesButton = $('#saveChanges');
+saveChangesButton.on("click", saveChanges)
